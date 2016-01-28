56b0eb055dca49f8d3c46fdcf180b323f03c48a1
Sourav Chakraborty <chakrabs@cse.osu.edu>
2016-01-27 22:08:14 -0500

    remove stopwords

--- a1/src/main.py
+++ a1/src/main.py
@@ -2,22 +2,46 @@ import os
 from os.path import join
 from bs4 import BeautifulSoup
 
-def process_file(fname):
-    s = BeautifulSoup(open(fname), 'lxml')
-    for doc in s.findAll('reuters'):
-        for e in doc:
-            print e
+input_dir  = '../data/input'
+temp_dir   = '../data/temp'
+output_dir = '../data/output'
+extension  = '.sgm'
+stopfile   = 'stopwords.txt'
+stopwords  = []
 
-def process_files(input_dir, temp_dir, output_dir, extension):
+def read_stopwords():
+    global stopwords
+    with open(join(input_dir, stopfile), 'r') as sf:
+        stopwords = sf.read().split()
+
+def remove_stopwords(text):
+    return ' '.join([w for w in text.split() if w not in stopwords])
+
+def find_children(doc, parent):
+    for child in doc.find(parent).find_all('d'):
+        yield child.string
+
+def process_file(fname):
+    print "Processing %s" % fname
+    soup = BeautifulSoup(open(fname), 'html.parser')
+    for doc in soup.find_all('reuters'):
+        id = doc['newid']
+        date = doc.find('date').string
+        title = doc.find('title').string
+        topics = list(find_children(doc, 'topics'))
+        places = list(find_children(doc, 'places'))
+        body = remove_stopwords(doc.find('body').string.lower())
+        print id, date, topics, places
+        print remove_stopwords(body)
+        print
+
+def process_files():
     print input_dir, temp_dir, output_dir, extension
     for fname in os.listdir(input_dir):
         if fname.endswith(extension):
-            process_file(join(input_dir,fname))
+            process_file(join(input_dir, fname))
 
 if __name__ == '__main__':
-    input_dir  = '../data/input'
-    temp_dir   = '../data/temp'
-    output_dir = '../data/output'
-    extension  = '.sgm'
 
-    process_files(input_dir, temp_dir, output_dir, extension)
+    read_stopwords()
+    process_files()
