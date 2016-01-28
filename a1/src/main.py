fa6b38b3d601cce2c450149b8d520787477410ec
Sourav Chakraborty <chakrabs@cse.osu.edu>
2016-01-28 17:28:23 -0500

    add stemming logic

--- a1/src/main.py
+++ a1/src/main.py
@@ -1,6 +1,7 @@
-import os
+import os,csv
 from os.path import join
 from bs4 import BeautifulSoup
+#from nltk.stem import SnowballStemmer
 
 input_dir  = '../data/input'
 temp_dir   = '../data/temp'
@@ -8,18 +9,21 @@ output_dir = '../data/output'
 extension  = '.sgm'
 stopfile   = 'stopwords.txt'
 stopwords  = []
+#stemmer    = SnowballStemmer("english")
 
 def read_stopwords():
     global stopwords
     with open(join(input_dir, stopfile), 'r') as sf:
         stopwords = sf.read().split()
 
+def join_words(words):
+    return ' '.join(words)
+
 def remove_stopwords(text):
-    return ' '.join([w for w in text.split() if w not in stopwords])
+    return [word for word in text.split() if word not in stopwords]
 
 def find_children(doc, parent):
-    for child in doc.find(parent).find_all('d'):
-        yield child.string
+    return [child.string for child in doc.find(parent).find_all('d')]
 
 def process_file(fname):
     print "Processing %s" % fname
@@ -28,11 +32,12 @@ def process_file(fname):
         id = doc['newid']
         date = doc.find('date').string
         title = doc.find('title').string
-        topics = list(find_children(doc, 'topics'))
-        places = list(find_children(doc, 'places'))
+        topics = find_children(doc, 'topics')
+        places = find_children(doc, 'places')
         body = remove_stopwords(doc.find('body').string.lower())
+        #body = map(stemmer.stem, body)
         print id, date, topics, places
-        print remove_stopwords(body)
+        print body
         print
 
 def process_files():
