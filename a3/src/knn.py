142de832f224f5dc28cb39419a4e84c99462bffa
Sourav Chakraborty <chakrabs@cse.osu.edu>
2016-03-28 00:54:59 -0400

    use ball tree for knn

--- a3/src/knn.py
+++ a3/src/knn.py
@@ -4,5 +4,5 @@ from sklearn.neighbors import KNeighborsClassifier
 class knn(classifier):
 
     def __init__(self, doc):
-        algo = KNeighborsClassifier()
+        algo = KNeighborsClassifier(n_neighbors=1, algorithm='ball_tree')
         classifier.__init__(self, algo, doc)
